e394e38dad2f53d179b933e4983d977a7ea3f694
e-aacosta <aldair.acostafm@gmail.com>
2023-08-25 16:38:38 -0600

    Add new style for buttons and functionality to change the month in the calendar

--- SeinfieldCalendar/MainWindow.xaml.cs
+++ SeinfieldCalendar/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,43 +22,95 @@ namespace SeinfieldCalendar
     /// </summary>
     public partial class MainWindow : Window
     {
-        int COLUMN_WIDTH = 40;
-        int ROW_HEIGHT = 40;
+        private readonly int COLUMN_WIDTH = 40;
+        private readonly int ROW_HEIGHT = 40;
+        private DateTime currentDate;
+        private readonly List<string> days = new List<string> { "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" };
+        private readonly Func<int, int, int> add = (a, b) => a + b;
+        private readonly Func<int, int, int> subtract = (a, b) => a - b;
 
         public MainWindow()
         {
             InitializeComponent();
             /// setCalendar();
-            
             createCalendar();
+        }
+
+        private void setStyles()
+        {
+
+            SolidColorBrush buttonBackgroundBrush = new SolidColorBrush(Colors.LightBlue);
+            buttonBackgroundBrush.Freeze();
+
+            Style buttonStyle = new Style(typeof(Button));
+            buttonStyle.Setters.Add(new Setter(Button.BackgroundProperty, buttonBackgroundBrush));
+            buttonStyle.Setters.Add(new Setter(Button.ForegroundProperty, Brushes.White));
+
+            Resources.Add("ButtonBackgroundBrush", buttonBackgroundBrush);
+            Resources.Add("ButtonStyle", buttonStyle);
         }
 
         private void createCalendar()
         {
+            setStyles();
+            currentDate = DateTime.Today;
             setCalendarColumns();
             setCalendarRows();
-            DateTime currentDate = DateTime.Today;
-            int daysOfMonth = DateTime.DaysInMonth(currentDate.Year,currentDate.Month);
-            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            setLabelDays();
 
-            monthLabel.Content = new DateTime(currentDate.Year, currentDate.Month, 1).ToString("MMMM");
+            monthLabel.Content = new DateTime(currentDate.Year, currentDate.Month, 1).ToString("yyyy MMM");
+            
+            setDaysInCalendar(currentDate);
 
-            for(int day = 1; day <= daysOfMonth; day++)
+
+            nextMonthButton.Click += (sender, e) => changeCurrentMonth(1);
+            prevMonthButton.Click += (senter, e) => changeCurrentMonth(-1);
+
+        }
+
+        private void setLabelDays()
+        {
+            int i = 0;
+            foreach (string d in days)
             {
-                int row = (day + (int)firstDayOfMonth.DayOfWeek - 1) / 7;
+                Label lbl = getLabelItem(d);
+                Grid.SetRow(lbl, 0);
+                Grid.SetColumn(lbl, i);
+                seinfieldCalendar.Children.Add(lbl);
+                i++;
+            }
+        }
+
+        private void changeCurrentMonth(int value)
+        {
+            DateTime nextMonth = currentDate.AddMonths(value);
+            monthLabel.Content = nextMonth.ToString("yyyy MMMM");
+            currentDate = nextMonth;
+            seinfieldCalendar.Children.Clear();
+            setLabelDays();
+            setDaysInCalendar(currentDate);
+            
+        }
+
+
+        private void setDaysInCalendar(DateTime monthCalendar)
+        {
+            int daysOfMonth = DateTime.DaysInMonth(monthCalendar.Year, monthCalendar.Month);
+            DateTime firstDayOfMonth = new DateTime(monthCalendar.Year, monthCalendar.Month, 1);
+
+            for (int day = 1; day <= daysOfMonth; day++)
+            {
+                int row = (day + (int)firstDayOfMonth.DayOfWeek - 1) / 7 + 1;
                 int column = ((day - 1) + (int)firstDayOfMonth.DayOfWeek) % 7;
 
                 Button date = getButtonItem(day);
-                Console.WriteLine(date.ToString());
 
-                
                 Grid.SetRow(date, row);
                 Grid.SetColumn(date, column);
                 seinfieldCalendar.Children.Add(date);
             }
 
-
-        }   
+        }
 
         private void setCalendarColumns()
         {
@@ -87,51 +141,29 @@ namespace SeinfieldCalendar
         }
         private void setCalendarRows()
         {
+            RowDefinition daysRow = new RowDefinition();
             RowDefinition week1 = new RowDefinition();
             RowDefinition week2 = new RowDefinition();
             RowDefinition week3 = new RowDefinition();
             RowDefinition week4 = new RowDefinition();
             RowDefinition week5 = new RowDefinition();
-            RowDefinition week6 = new RowDefinition();
+           /// RowDefinition week6 = new RowDefinition();
 
+            daysRow.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
             week1.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
             week2.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
             week3.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
             week4.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
             week5.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
-            week6.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
+            ///week6.Height = new GridLength(ROW_HEIGHT, GridUnitType.Pixel);
 
+            seinfieldCalendar.RowDefinitions.Add(daysRow);
             seinfieldCalendar.RowDefinitions.Add(week1);
             seinfieldCalendar.RowDefinitions.Add(week2);
             seinfieldCalendar.RowDefinitions.Add(week3);
             seinfieldCalendar.RowDefinitions.Add(week4);
             seinfieldCalendar.RowDefinitions.Add(week5);
-            seinfieldCalendar.RowDefinitions.Add(week6);
-        }
-
-        private void setCalendar()
-        {
-            setCalendarColumns();
-            setCalendarRows();
-
-            DateTime currentDate = DateTime.Now;
-            DateTime date = new DateTime(currentDate.Year, currentDate.Month, 1);
-            DayOfWeek dw = date.DayOfWeek;
-
-            //Here we gonna create an label and put it in the grid 
-            for(int i = 0 ; i< seinfieldCalendar.ColumnDefinitions.Count ; i++)
-            {
-                for(int j = 0;j<seinfieldCalendar.RowDefinitions.Count;j++)
-                {
-                    Button day = getButtonItem((int)(dw));
-                    seinfieldCalendar.Children.Add(day);
-                    Grid.SetColumn(day, i);
-                    Grid.SetRow(day, j);
-                 
-                }
-            }
-
-
+            //seinfieldCalendar.RowDefinitions.Add(week6);
         }
 
         //This will be every day on the calendar
@@ -139,15 +171,31 @@ namespace SeinfieldCalendar
         {
             Button n = new Button();
             n.Content = Convert.ToString(value);
+            n.Style = (Style)Resources["ButtonStyle"];
             n.HorizontalAlignment = HorizontalAlignment.Stretch;
             n.VerticalAlignment = VerticalAlignment.Stretch;
             n.Click += (sender, e) => dateClick(value);
             return n;
         }
 
+        private Label getLabelItem(string content)
+        {
+            Label n = new Label();
+            n.Content = content;
+            n.HorizontalAlignment = HorizontalAlignment.Center;
+            n.VerticalAlignment = VerticalAlignment.Center;
+            return n;
+        }
+
+
         private void dateClick(int day)
         {
             MessageBox.Show($"You click in the date {day}");
+        }
+
+        private void nextMonthButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 
